6ca16f9b77b9ab048cf56597a672da907bf63110
meres <rolaca12@gmail.com>
2017-03-06 06:20:11 -0800

    Resolved Issue: Joystick should work reversed

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -39,7 +39,7 @@ public class TrainControllerImpl implements TrainController {
 
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
-		this.step = -joystickPosition;
+		this.step = joystickPosition;
 	}
 
 }
